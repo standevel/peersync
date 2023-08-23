fc6820d093b1e955e9f557e2a2b715b181c1a539
Stanley Nwaegwu <kingstanley.ks16@gmail.com>
2023-08-23 18:08:20 +0100

    added build script that copies template folder to dist

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  // app.enableVersioning()
+  const config = new DocumentBuilder()
+    .setTitle('Workspace Service API')
+    .setDescription('The service is used to power the workspace app')
+    .setVersion('1.0')
+    .addTag('Workspace')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
-bootstrap();
+bootstrap(); 
